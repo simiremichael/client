ebd57ec083b527fe6320bc99102fb3de5a3f1629
Michael Simire <simiremichael@gmail.com>
2023-03-11 14:41:44 +0100

    updated

--- src/services/api/custormFetchBase.tsx
+++ src/services/api/custormFetchBase.tsx
@@ -5,7 +5,7 @@ import { setUsers, logoutUsers } from '../features/userSlice';
 import { setCompanies, companyLogout } from '../features/companySlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl:  process.env.REACT_APP_SERVER_URL || 'http://localhost:5000',
+  baseUrl:  process.env.REACT_APP_SERVER_URL || 'http://localhost:3000',
   credentials: 'include',
  // mode: 'no-cors',
   prepareHeaders: (headers, { getState }) => {
