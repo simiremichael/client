adff66ea76b1cb2d4d8385e165737f071a0c9327
Michael Simire <simiremichael@gmail.com>
2023-03-12 15:57:22 +0100

    updated

--- src/services/api/custormFetchBase.tsx
+++ src/services/api/custormFetchBase.tsx
@@ -6,7 +6,7 @@ import { setCompanies, companyLogout } from '../features/companySlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://server-beige-nine.vercel.app",
-  credentials: "same-origin",
+  credentials: "include",
   mode: "cors",
   prepareHeaders: (headers, { getState }) => {
     const agentToken = ( getState() as RootState).agentState.agentToken
