f35d0ed86455b716eb08f2116b8d03467c299df6
Michael Simire <simiremichael@gmail.com>
2023-03-14 18:24:52 +0100

    updated

--- src/pages/buyDetailsPage.tsx
+++ src/pages/buyDetailsPage.tsx
@@ -183,7 +183,6 @@ const CallContainer = styled.div`
 background-color: #008080;
 padding: 0 10px;
 margin: 5px;
-width: 30%;
 border-radius: 5px;
 display: flex;
 cursor: pointer;
@@ -195,7 +194,6 @@ const EmailContainer = styled.div`
 background-color: #008080;
 padding: 0 10px;
 margin: 5px;
-width: 30%;
 border-radius: 5px;
 display: flex;
 cursor: pointer;
@@ -207,7 +205,6 @@ const WhatsAppContainer = styled.div`
 background-color: #32CD32;
 padding: 0 10px;
 margin: 5px;
-width: 30%;
 border-radius: 5px;
 display: flex;
 cursor: pointer;
@@ -560,8 +557,7 @@ function BuyDetailsPage() {
         style={{width: '100%', 
         height: '100vh',
         }}
-     mapStyle="mapbox://styles/mapbox/streets-v9"
-    //mapStyle="mapbox://styles/simiremichael/clcz22d6e00l814qni4m9qxaq"
+    mapStyle="mapbox://styles/simiremichael/clcz22d6e00l814qni4m9qxaq"
     mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     onMove={((evt: any) => setViewState(evt))}
     >
@@ -661,7 +657,7 @@ function BuyDetailsPage() {
               <CallContainer>
                 <Svg3 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M511.2 387l-23.25 100.8c-3.266 14.25-15.79 24.22-30.46 24.22C205.2 512 0 306.8 0 54.5c0-14.66 9.969-27.2 24.22-30.45l100.8-23.25C139.7-2.602 154.7 5.018 160.8 18.92l46.52 108.5c5.438 12.78 1.77 27.67-8.98 36.45L144.5 207.1c33.98 69.22 90.26 125.5 159.5 159.5l44.08-53.8c8.688-10.78 23.69-14.51 36.47-8.975l108.5 46.51C506.1 357.2 514.6 372.4 511.2 387z" /></Svg3>
                 <Call><strong>CALL</strong></Call>
-              </CallContainer>]
+              </CallContainer>
               </Links>
                 <Links to={`mailto:${buyDetail?.email}`}>
               <EmailContainer>
